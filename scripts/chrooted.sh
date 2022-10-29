55825e59f160dc3352a099cc0e4a059315cd54be
suyog prasai <suyog224356@gmail.com>
2022-10-29 13:00:10 +0545

    Added new files and module to install microcode

--- scripts/chrooted.sh
+++ scripts/chrooted.sh
@@ -86,3 +86,21 @@ network_config() { # Network configuration
 }
 
 network_config
+
+# NOTE installating microcode
+
+# determine processor type and install microcode
+proc_type=$(lscpu)
+if grep -E "GenuineIntel" <<< ${proc_type}; then
+    echo "Installing Intel microcode"
+    pacman -S --noconfirm --needed intel-ucode
+    proc_ucode=intel-ucode.img
+elif grep -E "AuthenticAMD" <<< ${proc_type}; then
+    echo "Installing AMD microcode"
+    pacman -S --noconfirm --needed amd-ucode
+    proc_ucode=amd-ucode.img
+fi
+
+if [[ $INSTALL_TYPE == "MINIMAL" ]]; then
+   exit
+fi
