bf1c7d9f2e389c0aa219d43e143c2f7a68086af6
suyog <suyog224356@gmail.com>
2023-04-12 21:07:23 +0545

    Added module for install many pkgs at once pkg_install.sh

--- scripts/pkg_install.sh
+++ scripts/pkg_install.sh
@@ -1,2 +1,59 @@
 #! /usr/bin/env bash
 
+declare -a pkgs # Array declaration for installing packages
+
+PKG_LISTS_DIR=../pkglists
+INSTALL_TYPE=FULL
+
+
+pkg_dir=$PKG_LISTS_DIR
+default=${PKG_LISTS_DIR}/default
+
+cli_pkg=${default}/d_cli.txt
+gui_pkg=${default}/d_gui.txt
+combined_pkg=${pkg_dir}/combined.txt
+final_pkg=${pkg_dir}/final.txt
+
+if [ ${INSTALL_TYPE} == "MINIMAL" ]
+then 
+    cat ${cli_pkg} >> ${combined_pkg}
+elif [ ${INSTALL_TYPE} == "FULL" ]
+then 
+    cat ${cli_pkg} >> ${combined_pkg} 
+    cat ${gui_pkg} >> ${combined_pkg}
+    case $DESKTOP_ENV in 
+
+        awesome)
+            cat ${pkg_dir}/awesome.txt >> ${combined_pkg}
+            ;;
+        bspwm)
+            cat ${pkg_dir}/bspwm.txt >> ${combined_pkg}
+            ;;
+         qtile)           
+            cat ${pkg_dir}/qtile.txt >> ${combined_pkg}
+            ;;
+        xmonad)
+            cat ${pkg_dir}/xmonad.txt >> ${combined_pkg}
+            ;;
+        *)
+            ;;
+        esac
+fi
+sed '/^\#/d;/^[[:space:]]*$/d' ${combined_pkg} > ${final_pkg}
+rm ${combined_pkg}
+
+
+while read -r line; 
+do
+
+    pkgs+=("$(echo "$line")")
+done < ${final_pkg}
+
+do_install "${pkgs[@]}"
+
+
+
+
+
+
+
