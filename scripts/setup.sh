c1b612802ba3e018c910ec4d0fa40d0cf0834eb4
suyog <suyog224356@gmail.com>
2023-04-29 21:29:26 +0545

    fixed one thing

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -279,8 +279,8 @@ if [ -f ${CONFIG_FILE} ]; then
         Yes)
             background_checks # Does some background checks
             clear
-            pkg_setup      # Sets up pkgs required for the install
             display_config # Finally displays the config that is generated
+            pkg_setup      # Sets up pkgs required for the install
             ;;
         No)
             background_checks # Does some background checks
